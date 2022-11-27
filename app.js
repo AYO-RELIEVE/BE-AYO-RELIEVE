28ea1e683283d38a7cae6cf8fcab284a94953962
zakihaha <muhzakich@students.amikom.ac.id>
2022-11-27 13:40:28 +0700

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const authRouter = require('./routes/auth');
 const categoriesRouter = require('./routes/categories');
@@ -14,6 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+    origin: '*'
+}));
 
 app.use('/api/auth', authRouter);
 app.use('/api/categories', categoriesRouter);
