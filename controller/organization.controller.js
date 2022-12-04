042d5a3d6e7df7632dc5c5e49d966b16b208717e
zakihaha <muhzakich@students.amikom.ac.id>
2022-12-04 19:39:20 +0700

    fix: add additional data in programs

--- controller/organization.controller.js
+++ controller/organization.controller.js
@@ -3,16 +3,23 @@ const { Program, User } = require("../models")
 const listApplicant = async (req, res) => {
     const id = req.params.id;
     const program = await Program.findByPk(id, {
-        include: [{
-            model: User,
-            as: "applicant",
-            attributes: {
-                exclude: ["password"]
+        include: [
+            {
+                model: User,
+                as: "organization",
+                attributes: ["id", "name", "photo"]
             },
-            through: {
-                attributes: ["status"]
+            {
+                model: User,
+                as: "applicant",
+                attributes: {
+                    exclude: ["password"]
+                },
+                through: {
+                    attributes: ["status"]
+                }
             }
-        }]
+        ]
     });
 
     res.status(200).json({
@@ -21,11 +28,17 @@ const listApplicant = async (req, res) => {
 }
 
 const listProgram = async (req, res) => {
-    console.log("id", req.user.id)
     const programs = await Program.findAll({
         where: {
             organization_id: req.user.id
-        }
+        },
+        include: [
+            {
+                model: User,
+                as: "organization",
+                attributes: ["id", "name", "photo"]
+            }
+        ]
     })
 
     res.status(200).json({
